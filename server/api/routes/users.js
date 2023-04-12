527006415535eaac34531d798290e521af704964
Mahfuzur Rahman <smmahfuz787@gmail.com>
2023-04-12 13:56:00 +0600

    JWT token implement to verify the valid user

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("Hello you are logined");
+})
 
 //UPDATE
 router.put("/:id", updateUser)
--- server/api/utils/verifyToken.js
+++ server/api/utils/verifyToken.js
@@ -1,1 +1,17 @@
+import jwt from "jsonwebtoken";
+import { createError } from "../utils/error.js";
 
+
+export const verifyToken = (req, res, next)=>{
+    const token = req.cookies.access_token
+    if (!token) {
+        return next(createError(401," you are not authorize"));
+    }
+
+    jwt.verify(token, process.env.JWT, (err, user) => {
+        if (err) return next(createError(403, "Token is not valid"));
+        req.user = user;
+        next();
+    })
+
+}

89dafe3bc0714dec986d964cc043b7d3625e0038
Mahfuzur Rahman <smmahfuz787@gmail.com>
2023-04-12 13:05:07 +0600

    Create User Operation

--- server/api/controllers/user.js
+++ server/api/controllers/user.js
@@ -1,15 +1,4 @@
-import User from "../models/User.js"
-
-export const createUser = async (req, res, next) => {
-    const newUser = new User(req.body)
-
-    try {
-        const saveUser = await newUser.save()
-        res.status(200).json(saveUser)
-    } catch (err) {
-        next(err)
-    }
-}
+import User from "../models/User.js";
 export const updateUser = async (req, res, next) => {
     try {
         const updatedUser = await User.findByIdAndUpdate(
